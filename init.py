7ea0fb9757e21a67d696b64e35c84b24f582df1c
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-21 11:10:19 -0700

    Update init.py

--- init.py
+++ init.py
@@ -1,37 +1,37 @@
 from flask import Flask, render_template, request
-import uuid
-import base64
 import os
 import json
-def post_json(req):
-    if req == "":
+with open("data.json") as file:
+        jsonOBJ =  json.load(file)
+jsonOBJ = json.dumps(jsonOBJ, indent=2)
+file.close()
+def post_json(idty,confession):
+    if confession == None or idty == None:
         pass
     else:
         with open("data.json") as fp:
             listObj = json.load(fp)
             fp.close()
             listObj["entries"].append({
-                "id" :{
-                "confession" : req
+                idty :{
+                "confession" : confession
                 }})
         with open("data.json", 'w') as json_file:
-            json.dump(listObj, json_file, 
-                                indent=4,  
-                                separators=(',',': '))
+            json.dump(listObj, json_file,indent=4,separators=(',',': '))
         json_file.close()
-            
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template("index.html",uuid=uuid.uuid4())
+    return render_template("index.html")
 @app.route('/', methods=['POST','GET'])
 def submit():
     if request.method == "POST":
-        text = request.form["nm"]
-        post_json(text)
-        return render_template("index.html",uuid=uuid.uuid4())
+        confession = request.form["confession"]
+        idty = request.form["identity_send"]
+        post_json(idty,confession)
+        return render_template("index.html")
     else:
-        return render_template("index.html",uuid=uuid.uuid4())
+        return render_template("index.html")
 if __name__ == '__main__':
     app.run(debug=True,host="10.159.13.217")
 
