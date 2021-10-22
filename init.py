b33cc5a5fd740c9148d6191e4a9792a15c1208c3
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-22 11:23:11 -0700

    Update init.py
    Updated the syntax and made it functioning again

--- init.py
+++ init.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request
 import os
 import json
-with open("data.json") as file:
-        jsonOBJ =  json.load(file)
-jsonOBJ = json.dumps(jsonOBJ, indent=2)
-file.close()
 def post_json(idty,confession):
     if confession == None or idty == None:
         pass
@@ -13,25 +9,30 @@ def post_json(idty,confession):
             listObj = json.load(fp)
             fp.close()
             listObj["entries"].append({
-                idty :{
+                "index" :{
+                "identity" : idty,    
                 "confession" : confession
                 }})
         with open("data.json", 'w') as json_file:
             json.dump(listObj, json_file,indent=4,separators=(',',': '))
         json_file.close()
+def ReturnList():
+    with open("data.json") as fp:
+        listObj = json.load(fp)
+        fp.close()
+        return_json = listObj
+    return return_json
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",json=ReturnList())
 @app.route('/', methods=['POST','GET'])
 def submit():
     if request.method == "POST":
         confession = request.form["confession"]
         idty = request.form["identity_send"]
         post_json(idty,confession)
-        return render_template("index.html")
+        return render_template("index.html",json=ReturnList())
     else:
-        return render_template("index.html")
-if __name__ == '__main__':
-    app.run(debug=True,host="10.159.13.217")
+        return render_template("index.html",json=ReturnList())
 
