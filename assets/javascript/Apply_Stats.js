9bc87137f012fa9909c48e254508c1625a3bafb2
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-24 00:58:21 -0500

    Create Apply_Stats.js
    This allows easy extraction of the values, and makes it hard to insert values upon submit to sabotage it

---
+++ assets/javascript/Apply_Stats.js
@@ -0,0 +1,6 @@
+	function Apply_Stats() {
+		console.log(identity_made[0])
+		document.getElementById("identity_send").value = identity_made[0]
+      document.getElementById("size_send").value = document.getElementById("size").value
+      document.getElementById("color_send").value = document.getElementById("color").value
+	}
