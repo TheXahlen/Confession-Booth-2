3fa76d32523316b4d1aa898d8b5be43cb7dff298
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-24 08:37:54 -0700

    Create identity_assign.js

---
+++ static/assets/javascript/identity_assign.js
@@ -0,0 +1,15 @@
+function get_identity(ip) {
+  var ip = ip.split(".");
+  let base_ip = parseInt(ip.join("6"));
+  base_ip = base_ip * 5;
+  let shuffled_base = btoa(base_ip);
+  shuffled_base = shuffled_base.substring(0, shuffled_base.length / 2);
+  document.getElementById("identity").innerHTML = shuffled_base
+    .split()
+    .reverse();
+  return shuffled_base.split().reverse();
+}
+$.getJSON("https://api.ipify.org/?format=json", function (e) {
+  get_identity(e.ip);
+  window.identity_made = get_identity(e.ip);
+});
