d0974e8b3d60c2a431603258febc09c6f3e26eca
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-24 00:55:44 -0500

    Update update_display.js
    Added the color variables and it also now stores in a dict for easy indexing

--- assets/javascript/update_display.js
+++ assets/javascript/update_display.js
@@ -1,16 +1,26 @@
-var myObject = document.getElementById("confession_items").innerHTML
-JSONarray = []
-myObject = JSON.parse(myObject)
-
-length = Object.keys(myObject['entries'])
-length = length.length
-var myObject = document.getElementById("confession_items").innerHTML = ""
-for (let i = 0; i < length; i++) {
-   console.log(i)
-   let confession = myObject['entries'][i]["index"]["confession"]
-   let identiy = myObject['entries'][i]["index"]["identity"]
-   JSONarray.push(confession + " Posted by: " + identiy)
-function UpdateDisplay(){
-   var item = JSONarray[Math.floor(Math.random()*JSONarray.length)];
-   document.getElementById("confession_display").innerHTML = item
-}}
+var myObject =  '{{ json| tojson | safe }}'
+	myObject = JSON.parse(myObject)
+	var length = Object.keys(myObject['entries'])
+	length = length.length
+   let empty = []
+	for(let i = 0; i < length; i++) {
+		console.log(i)
+		let confession = myObject['entries'][i]["index"]["confession"]
+		let identiy = myObject['entries'][i]["index"]["identity"]
+      let size = myObject['entries'][i]["index"]["size"]
+      let color = myObject['entries'][i]["index"]["color"]
+      let confession_complete = confession + " Posted by: " + identiy
+      empty.push({
+         confession:  confession_complete,
+         size: size,
+         color : color 
+      });
+      console.log(empty)
+		function UpdateDisplay() {
+         number_created = Gen_Number(empty)
+			var item =  empty[number_created].confession;
+			document.getElementById("confession_display").innerHTML = item
+		}
+	}
+	UpdateDisplay()
+	$("#confession_items").remove();
