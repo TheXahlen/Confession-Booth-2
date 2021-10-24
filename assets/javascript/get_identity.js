305fbe6ae1bc3a3aa7a86b7e65e2b54d7f7614df
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-24 00:57:34 -0500

    Update get_identity.js
    Now has the identity as a global window variable to be accessed by the Apply_Stats() Function

--- assets/javascript/get_identity.js
+++ assets/javascript/get_identity.js
@@ -1,13 +1,16 @@
-function get_identity(ip) {
-    var ip = ip.split(".")
-    let base_ip = parseInt(ip.join("6"))
-   // console.log(base_ip)
-    base_ip = base_ip * 5
-   // console.log(base_ip)
-    let shuffled_base = btoa(base_ip)
-    shuffled_base = shuffled_base.substring(0, shuffled_base.length / 2 )
-    document.getElementById("identity").innerHTML = shuffled_base.split().reverse()
-}
-$.getJSON("https://api.ipify.org/?format=json", function(e) {
-    get_identity(e.ip);
-});
+	function get_identity(ip) {
+		var ip = ip.split(".")
+		let base_ip = parseInt(ip.join("6"))
+			// console.log(base_ip)
+		base_ip = base_ip * 5
+			// console.log(base_ip)
+		let shuffled_base = btoa(base_ip)
+		shuffled_base = shuffled_base.substring(0, shuffled_base.length / 2)
+		document.getElementById("identity").innerHTML = shuffled_base.split().reverse()
+			// document.getElementById("identity_send").value = shuffled_base.split().reverse()
+		return shuffled_base.split().reverse()
+	}
+	$.getJSON("https://api.ipify.org/?format=json", function(e) {
+		get_identity(e.ip);
+		window.identity_made = get_identity(e.ip)
+	});
