659d58a76da4c03dcbb17c5c5bbac5599e4bca5e
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-24 08:31:47 -0700

    Delete assets directory

--- assets/javascript/Apply_Stats.js
+++
@@ -1,6 +0,0 @@
-	function Apply_Stats() {
-		console.log(identity_made[0])
-		document.getElementById("identity_send").value = identity_made[0]
-      document.getElementById("size_send").value = document.getElementById("size").value
-      document.getElementById("color_send").value = document.getElementById("color").value
-	}
