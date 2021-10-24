45eebae73c626a85ffeca57b3c4a733974b963d7
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-24 00:56:28 -0500

    Update switch_button.js
    Now shows the selector dropdowns for size and color

--- assets/javascript/switch_button.js
+++ assets/javascript/switch_button.js
@@ -1,14 +1,17 @@
-  $(function(){
-           $('#Switch').click(function() {
-       if($("#Cycle").prop("disabled")){
-       $("#Cycle").prop("disabled",false);
-       $("#Submit").prop("disabled",true);
-       $('#submit_form').attr("style", "display: none !important");
-       $('#cycle_form').attr("style", "visibility: visible !important");
-       }
-       else{
-       $("#Cycle").prop("disabled",true);
-       $("#Submit").prop("disabled",false);
-       $('#submit_form').attr("style", "visibility: visible !important");
-       $('#cycle_form').attr("style", "display:none !important");
-       }});});
+$(function() {
+		$('#Switch').click(function() {
+			if($("#Cycle").prop("disabled")) {
+				$("#Cycle").prop("disabled", false);
+				$("#Submit").prop("disabled", true);
+				$('#submit_form').attr("style", "display: none !important");
+				$('#cycle_form').attr("style", "visibility: visible !important");
+				$('#cust_select').attr("style", "display: none !important");
+			} else {
+				$("#Cycle").prop("disabled", true);
+				$("#Submit").prop("disabled", false);
+				$('#submit_form').attr("style", "visibility: visible !important");
+				$('#cycle_form').attr("style", "display:none !important");
+				$('#cust_select').attr("style", "visibility: visible !important");
+			}
+		});
+	});
