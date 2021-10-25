a23014ca6f74374c1d7ac992ed59ef43b0d60ce5
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-24 17:33:20 -0700

    Update apply_stats.js

--- static/assets/javascript/apply_stats.js
+++ static/assets/javascript/apply_stats.js
@@ -3,4 +3,6 @@ function Apply_Stats() {
     document.getElementById("identity_send").value = identity_made[0];
     document.getElementById("size_send").value = document.getElementById("size").value;
     document.getElementById("color_send").value = document.getElementById("color").value;
+    document.getElementById("decoration_send").value = document.getElementById("decoration").value;
+    document.getElementById("time_send").value = Get_Time()
   }
