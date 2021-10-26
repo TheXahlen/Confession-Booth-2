adf5fa04486fbb599c0948e6b2c669acfac4a5df
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-26 08:39:38 -0700

    Update check_sim.js

--- static/assets/javascript/check_sim.js
+++ static/assets/javascript/check_sim.js
@@ -3,6 +3,7 @@ var current_posts = Get_All_Posts(identity_made)
 for (let i = 0; i < current_posts.length; i++) {
 var similarity = current_posts[i].confession
 var similarity_of_item = stringSimilarity.compareTwoStrings($('#confession_input').val().toLowerCase(), similarity.toLowerCase())
+console.log(similarity_of_item)
 if (similarity_of_item > 0.74){
 return false
 }else {
