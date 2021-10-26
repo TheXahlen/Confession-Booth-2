2d5adbf514ef5acb7bfb0a1711793a0d8ad41693
Dahlen Bryce <47333926+TheXahlen@users.noreply.github.com>
2021-10-26 07:42:19 -0700

    Update switch_input.js

--- static/assets/javascript/switch_input.js
+++ static/assets/javascript/switch_input.js
@@ -1,17 +1,18 @@
 $("#Switch").click(function() {
-    if ($("#Cycle").prop("disabled")) {
-        $("#Cycle").prop("disabled", false);
-        $("#Submit").prop("disabled", true);
-        $('#submit_form').attr("style", "display:none !important");
-        $('#cycle_form, #total_display, #all_posts, #confession').attr("style", "visibility: visible !important");
-        UpdateDisplay();
-    } else {
-        $("#Cycle").prop("disabled", true);
-        $("#Submit").prop("disabled", false);
-        console.log("VISIBILE");
-        $('#submit_form').attr("style", "");
-        $("#cust_select").attr("style", "");
-        $('#all_posts, #cust_select, #total_display,#cycle_form, #confession').attr("style", "display:none !important");
-        $('#confession_all').empty();
-    }
-});
+	if ($("#Cycle").prop("disabled")) {
+	$("#Cycle").prop("disabled", false);
+	$("#Submit").prop("disabled", true);
+	$('#submit_form').attr("style", "display:none !important");
+	$('#cycle_form, #total_display, #all_posts, #confession').attr("style", "visibility: visible !important");
+  UpdateDisplay();
+
+
+	} else {
+	$("#Cycle").prop("disabled", true);
+	$("#Submit").prop("disabled", false);;
+	$('#submit_form').attr("style", "");
+	$("#cust_select").attr("style","");
+    $('#all_posts, #cust_select, #total_display,#cycle_form, #confession').attr("style", "display:none !important");
+    $('#confession_all').empty();
+	
+	}});
